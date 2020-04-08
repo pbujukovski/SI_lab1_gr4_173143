a8837b2ab8970606ffb447441fb336b7f9124539
pbujukovski <62770688+pbujukovski@users.noreply.github.com>
2020-04-08 21:46:06 +0200

    point class finished

--- Point.java
+++ Point.java
@@ -5,15 +5,36 @@ class Point {
 	//TODO add new variable
 	string color;
 	//TODO constructor
+	 Point(id,x,y,color){
+		strcpy(this->adresa,adresa);
+		this->x=x;
+		this->y=y;
+		this->color=color;
 
+		}
 	//TODO setters and getters
+		string *getId(){
+     		return id;
+    		}	
+
+		string *getColor(){
+     		return color;
+    		}	
+
 
 	public void move (char xDirection, char yDirection) {
 		//TODO
+		if (id== xDiricton(L)) return left;
+		else if (id== xDiricton(R)) return right;
+		else if (id== xDiricton(D)) return down;
+		else return top;	
 	}
 
 	public void draw () {
 		//TODO
+		for(int i=0; i<id; i++){
+			syste.out.prinln("tockata vo momentot e " + id);
+		}
 	}
 
 
